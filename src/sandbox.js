525ed34e40fcad9a8be68d6bf0203017f5d683f4
Mina Shahrokhnia <mina.shahrokhnia@rdsysco.com>
2019-05-15 06:29:36 +0430

    should call core find element

--- src/sandbox.js
+++ src/sandbox.js
@@ -3,6 +3,6 @@ export default class Sandbox{
     this._core = core;
   }
   findElement () {
-
+    this._core.getExtension('$').find();
   }
 }
--- test/sandbox.test.js
+++ test/sandbox.test.js
@@ -1,6 +1,8 @@
 import Core from "../src/core";
 import Sandbox from '../src/sandbox';
 
+//jest.mock('../src/core');
+
 describe('sandbox', () => {
   it('should accept core in the constructor', () => {
     const core = new Core();
@@ -11,6 +13,16 @@ describe('sandbox', () => {
   it('shoud have find element', () => {
     const sandbox = new Sandbox();
     Sandbox.findElement = jest.fn();
+    
     expect(sandbox.findElement).toBeDefined();
+  })
+
+  it('should call core find element', () => {
+    const core = new Core();
+    core.use('$', {find: jest.fn()});
+    const sandbox = new Sandbox(core);
+    sandbox.findElement();
+    
+    expect(sandbox._core.getExtension('$').find).toHaveBeenCalled();
   })
 });
